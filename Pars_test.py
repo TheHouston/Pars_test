e06a5e4c005c90a5310cad5473640168ae3fcf5b
Михаил Букин <aaae2011@mail.ru>
2022-03-22 17:20:47 +0300

    Изменил продход к решению проблемы

--- Pars_test.py
+++ Pars_test.py
@@ -1,12 +1,10 @@
+from pprint import pprint
 #Пути к файлам
 #path_1 = input ('Путь к первому файлу: ')
 #path_2 = input ('Путь ко второму файлу: ')
 path_1 = 'F:\\Общая\\test.txt'
 path_2 = 'F:\\Общая\\test2.txt'
 path_3 = 'F:\\Общая\\test3.txt'
-
-#Счетчики для секций
-count = caunt = 0
 
 #Открываем три файла
 with open(path_1, 'r') as file_1, open(path_2, 'r') as file_2, open(path_3, 'w') as file_3:
@@ -18,8 +16,8 @@ with open(path_1, 'r') as file_1, open(path_2, 'r') as file_2, open(path_3, 'w')
     quest_section_1 = file_1_r.split('#\n')
     quest_section_2 = file_2_r.split('#\n')
 
-    #Перебор значений из списков
-    for section_1 in quest_section_1:
+        #Перебор значений из списков
+    '''for section_1 in quest_section_1:
         for section_2 in quest_section_2:
             #Убрал первые 15 символов и сравниваю строковые значения
             if section_1[15:] == section_2[15:]:
@@ -33,7 +31,15 @@ with open(path_1, 'r') as file_1, open(path_2, 'r') as file_2, open(path_3, 'w')
                 file_3.write(section_1[15::])
                 print (section_1)
                 print (section_2)
-                caunt += 1
-print(count)
-print(caunt)
-            
+                caunt += 1'''
+    for section_1 in quest_section_1:
+        try:
+            quest_section_2.remove(section_1)
+            pprint (quest_section_2)
+        except ValueError:
+            #continue
+            file_3.write('#\n')
+            file_3.write(section_1)
+    for section_2 in quest_section_2:
+        file_3.write('#\n')
+        file_3.write(section_2)
