d4966d94e686d7055d0744406e55c62057b16ee2
Михаил Букин <aaae2011@mail.ru>
2022-03-24 10:23:38 +0300

    Чистка кода

--- Pars_test.py
+++ Pars_test.py
@@ -5,6 +5,8 @@ path_1 = 'F:\\Общая\\003.90(сборка).txt'
 path_2 = 'F:\\Общая\\003.90.txt'
 path_3 = 'C:\\Users\\mbukin\\Desktop\\test3.txt'
 
+voprosov = 0
+
 #Открываем три файла
 with open(path_1, 'r') as file_1, open(path_2, 'r') as file_2, open(path_3, 'w') as file_3:
     #Читаем первые два файла
@@ -12,23 +14,29 @@ with open(path_1, 'r') as file_1, open(path_2, 'r') as file_2, open(path_3, 'w')
     file_2_r = file_2.read()
 
     #Создание списков из файлов
-    quest_section_1 = file_1_r.split('#\n')
-    quest_section_2 = file_2_r.split('#\n')
-
+    question_section_1 = file_1_r.split('#\n')
+    question_section_2 = file_2_r.split('#\n')
+    print(len(quest_section_2))
         #Перебор значений из списка
-    for section_1 in quest_section_1:
-        for section_2 in quest_section_2:
-            sect_1 = section_1
-            sect_2 = section_2
+    for section_1 in question_section_1:
+        for section_2 in question_section_2:
+            sec_1 = section_1.split('\n')
+            sec_2 = section_2.split('\n')
             try:
-                number_1 = sect_1[:15]
-                number_2 = section_2[:15]
-                if sect_1[15:] == sect_2[15:]:
+                dlina_1 = len(sec_1[0])
+                dlina_2 = len(sec_2[0])
+                number_1 = section_1[:dlina_1]
+                number_2 = section_2[:dlina_2]
+                if section_1[dlina_1:] == section_2[dlina_2:]:                    
                     section_1 = section_1.replace(number_1, number_2)
-                    quest_section_2.remove(section_1)
+                    question_section_2.remove(section_1)
+                    print('Удалил')
             except ValueError:
-                file_3.write('#\n')
+                file_3.write('#добавление\n')
                 file_3.write(section_1)
-    for section_2 in quest_section_2:
-        file_3.write('#\n')
+                voprosov += 1
+    for section_2 in question_section_2:
+        file_3.write('#итоговый список\n')
         file_3.write(section_2)
+voprosov += len(quest_section_2)
+print (voprosov)
