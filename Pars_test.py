2e4ef13136e5e7810aba3b938e48242633911e74
Михаил Букин <aaae2011@mail.ru>
2022-03-28 13:47:41 +0300

    исправил несколко ошибок

--- Pars_test.py
+++ Pars_test.py
@@ -5,38 +5,43 @@ path_1 = 'F:\\Общая\\003.90(сборка).txt'
 path_2 = 'F:\\Общая\\003.90.txt'
 path_3 = 'C:\\Users\\mbukin\\Desktop\\test3.txt'
 
-voprosov = 0
+counter_voprosov = 0
+
+def write_in_file (info):
+    file_3.write('#\n')
+    file_3.write(info)
 
 #Открываем три файла
 with open(path_1, 'r') as file_1, open(path_2, 'r') as file_2, open(path_3, 'w') as file_3:
+
     #Читаем первые два файла
-    file_1_r = file_1.read()
-    file_2_r = file_2.read()
+    file_1_r = file_1.read().lower()
+    file_2_r = file_2.read().lower()
 
     #Создание списков из файлов
     question_section_1 = file_1_r.split('#\n')
     question_section_2 = file_2_r.split('#\n')
-    print(len(quest_section_2))
-        #Перебор значений из списка
+    print('Количество вопросов: ', len(question_section_2))
+
+    #Перебор значений из списка
     for section_1 in question_section_1:
         for section_2 in question_section_2:
-            sec_1 = section_1.split('\n')
-            sec_2 = section_2.split('\n')
+            list_section_1 = section_1.split('\n')
+            list_section_2 = section_2.split('\n')
             try:
-                dlina_1 = len(sec_1[0])
-                dlina_2 = len(sec_2[0])
+                #Определение длины строки с номером вопроса
+                dlina_1 = len(list_section_1[0])
+                dlina_2 = len(list_section_2[0])
+                #Сохранение строки номера вопроса
                 number_1 = section_1[:dlina_1]
                 number_2 = section_2[:dlina_2]
                 if section_1[dlina_1:] == section_2[dlina_2:]:                    
                     section_1 = section_1.replace(number_1, number_2)
                     question_section_2.remove(section_1)
-                    print('Удалил')
+                    print ('Удалил ', number_2)
             except ValueError:
-                file_3.write('#добавление\n')
-                file_3.write(section_1)
-                voprosov += 1
+                continue
     for section_2 in question_section_2:
-        file_3.write('#итоговый список\n')
-        file_3.write(section_2)
-voprosov += len(quest_section_2)
-print (voprosov)
+        write_in_file(section_2)
+counter_voprosov += len(question_section_2)
+print ('В общем файле еще нет ', counter_voprosov, ' вопросов')
